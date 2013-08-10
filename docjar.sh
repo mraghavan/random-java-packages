e434b38baf311aa9b177c57179e51e71af99e2aa
Manish Raghavan <manishraghavan@gmail.com>
2013-08-10 12:37:30 -0700

    Usage, better style for docjar

--- docjar.sh
+++ docjar.sh
@@ -2,46 +2,50 @@
 #docjar.sh
 # compiles, javadocs, and jars a package
 
+USAGE="usage: ${BASH_SOURCE[0]} [ -h | --help] [package_name]"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PACKPATH=$DIR
-JARPATH=$DIR/jars
+PACKPATH=${DIR}
+JARPATH=${DIR}/jars
 PACKNAME=$1
-DOCPATH=$PACKPATH/$PACKNAME/docs
-SOURCEPATH=$PACKPATH/$PACKNAME/src
-OUTPATH=$PACKPATH/$PACKNAME/
+DOCPATH=${PACKPATH}/${PACKNAME}/docs
+SOURCEPATH=${PACKPATH}/${PACKNAME}/src
+OUTPATH=${PACKPATH}/${PACKNAME}/
+
+[[ "${PACKNAME}" == "-h" ||"${PACKNAME}" == "--help" ]] && echo ${USAGE} && exit 0
 
 compile()
 {
-	cd $SOURCEPATH
-	find . -name \*.java -exec javac -d $OUTPATH {} \+
+	cd ${SOURCEPATH}
+	find . -name \*.java -exec javac -d ${OUTPATH} {} \+
 	return $?
 }
 
 doc()
 {
-	cd $SOURCEPATH
-	javadoc -d $DOCPATH -subpackages $PACKNAME
+	cd ${SOURCEPATH}
+	javadoc -d ${DOCPATH} -subpackages ${PACKNAME}
 	return $?
 }
 
 jjar()
 {
-	cd $PACKPATH/$PACKNAME
-	jar cvf $JARPATH/$PACKNAME.jar $PACKNAME src docs || return 6
+	cd ${PACKPATH}/${PACKNAME}
+	jar cvf ${JARPATH}/${PACKNAME}.jar ${PACKNAME} src docs || return 6
 	return $?
 }
 
 
 
-[ -z $PACKNAME ] && echo Provide a package name && exit 1
+[ -z ${PACKNAME} ] && echo ${USAGE} && exit 1
 
-[ ! -d $PACKPATH/$PACKNAME ] && echo Invalid package $PACKPATH/$PACKNAME && exit 2
+[ ! -d ${PACKPATH}/${PACKNAME} ] && echo Invalid package ${PACKPATH}/${PACKNAME} && exit 2
 
-[ ! -d $SOURCEPATH ] && echo Put .java files in $SOURCEPATH && exit 3
+[ ! -d ${SOURCEPATH} ] && echo Put .java files in ${SOURCEPATH} && exit 3
 
 
 echo Compiling...
-find $OUTPATH -name \*.class -exec rm {} \;
+find ${OUTPATH} -name \*.class -exec rm {} \;
 compile
 if [[ $? != 0 ]]
 then
@@ -50,7 +54,7 @@ then
 fi
 echo Compile complete.; echo
 
-[ ! -e $DOCPATH ] && mkdir $DOCPATH
+[ ! -e ${DOCPATH} ] && mkdir ${DOCPATH}
 echo Generating javadocs...
 doc
 if [[ $? != 0 ]]
@@ -60,12 +64,12 @@ then
 fi
 echo javadocs generated.; echo
 
-echo Creating $PACKNAME.jar...
+echo Creating ${PACKNAME}.jar...
 jjar
 if [[ $? != 0 ]]
 then
 	echo jar failed.
 	exit 8
 fi
-echo $PACKNAME.jar created
+echo ${PACKNAME}.jar created
 exit 0
